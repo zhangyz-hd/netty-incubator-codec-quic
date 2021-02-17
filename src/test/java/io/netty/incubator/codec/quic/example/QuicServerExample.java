2fe69003a49c98fc4eb37df80169308f20ceaf06
Norman Maurer <norman_maurer@apple.com>
2021-02-17 15:31:26 +0100

    server code

--- src/test/java/io/netty/incubator/codec/quic/example/QuicServerExample.java
+++ src/test/java/io/netty/incubator/codec/quic/example/QuicServerExample.java
@@ -17,14 +17,19 @@ package io.netty.incubator.codec.quic.example;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.FixedRecvByteBufAllocator;
+import io.netty.channel.WriteBufferWaterMark;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioDatagramChannel;
-import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.ByteToMessageDecoder;
 import io.netty.handler.ssl.util.SelfSignedCertificate;
 import io.netty.incubator.codec.quic.InsecureQuicTokenHandler;
 import io.netty.incubator.codec.quic.QuicChannel;
@@ -32,16 +37,17 @@ import io.netty.incubator.codec.quic.QuicServerCodecBuilder;
 import io.netty.incubator.codec.quic.QuicSslContext;
 import io.netty.incubator.codec.quic.QuicSslContextBuilder;
 import io.netty.incubator.codec.quic.QuicStreamChannel;
-import io.netty.util.CharsetUtil;
 import io.netty.util.internal.logging.InternalLogger;
 import io.netty.util.internal.logging.InternalLoggerFactory;
 
 import java.net.InetSocketAddress;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public final class QuicServerExample {
 
     private static final InternalLogger LOGGER = InternalLoggerFactory.getInstance(QuicServerExample.class);
+    private static final int CHUNK_SIZE = 2048;
 
     private QuicServerExample() { }
 
@@ -49,28 +55,26 @@ public final class QuicServerExample {
         SelfSignedCertificate selfSignedCertificate = new SelfSignedCertificate();
         QuicSslContext context = QuicSslContextBuilder.forServer(
                 selfSignedCertificate.privateKey(), null, selfSignedCertificate.certificate())
-                .applicationProtocols("http/0.9").build();
+                .applicationProtocols("perf").build();
         NioEventLoopGroup group = new NioEventLoopGroup(1);
         ChannelHandler codec = new QuicServerCodecBuilder().sslContext(context)
                 .maxIdleTimeout(5000, TimeUnit.MILLISECONDS)
                 // Configure some limits for the maximal number of streams (and the data) that we want to handle.
-                .initialMaxData(10000000)
-                .initialMaxStreamDataBidirectionalLocal(1000000)
-                .initialMaxStreamDataBidirectionalRemote(1000000)
+                .initialMaxData(100000)
+                .initialMaxStreamDataBidirectionalLocal(10000)
+                .initialMaxStreamDataBidirectionalRemote(10000)
                 .initialMaxStreamsBidirectional(100)
                 .initialMaxStreamsUnidirectional(100)
-
+                .maxRecvUdpPayloadSize(1350)
+                .maxSendUdpPayloadSize(1350)
+                .activeMigration(false)
+                .earlyData(true)
                 // Setup a token handler. In a production system you would want to implement and provide your custom
                 // one.
                 .tokenHandler(InsecureQuicTokenHandler.INSTANCE)
                 // ChannelHandler that is added into QuicChannel pipeline.
                 .handler(new ChannelInboundHandlerAdapter() {
                     @Override
-                    public void channelActive(ChannelHandlerContext ctx) {
-                        QuicChannel channel = (QuicChannel) ctx.channel();
-                        // Create streams etc..
-                    }
-
                     public void channelInactive(ChannelHandlerContext ctx) {
                         ((QuicChannel) ctx.channel()).collectStats().addListener(f -> {
                             if (f.isSuccess()) {
@@ -87,21 +91,43 @@ public final class QuicServerExample {
                 .streamHandler(new ChannelInitializer<QuicStreamChannel>() {
                     @Override
                     protected void initChannel(QuicStreamChannel ch)  {
-                        // Add a LineBasedFrameDecoder here as we just want to do some simple HTTP 0.9 handling.
-                        ch.pipeline().addLast(new LineBasedFrameDecoder(1024))
-                                .addLast(new ChannelInboundHandlerAdapter() {
+                        ch.pipeline().addLast(new ByteToMessageDecoder() {
+                            private int numBytesRequested = -1;
+
                             @Override
-                            public void channelRead(ChannelHandlerContext ctx, Object msg) {
-                                ByteBuf byteBuf = (ByteBuf) msg;
-                                try {
-                                    if (byteBuf.toString(CharsetUtil.US_ASCII).trim().equals("GET /")) {
-                                        ByteBuf buffer = ctx.alloc().directBuffer();
-                                        buffer.writeCharSequence("Hello World!\r\n", CharsetUtil.US_ASCII);
-                                        // Write the buffer and shutdown the output by writing a FIN.
-                                        ctx.writeAndFlush(buffer).addListener(QuicStreamChannel.SHUTDOWN_OUTPUT);
+                            protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
+                                if (in.readableBytes() < 4) {
+                                    return;
+                                }
+                                if (numBytesRequested == -1) {
+                                    numBytesRequested = in.readInt();
+                                    writeData(ctx);
+                                } else {
+                                    in.skipBytes(in.readableBytes());
+                                }
+                            }
+
+                            private void writeData(ChannelHandlerContext ctx) {
+                                ByteBuf buffer = ctx.alloc().directBuffer(CHUNK_SIZE).writeZero(CHUNK_SIZE);
+                                do {
+                                    int size = Math.min(numBytesRequested, CHUNK_SIZE);
+                                    buffer.writerIndex(size);
+
+                                    ChannelFuture f = ctx.write(buffer.retainedDuplicate());
+                                    numBytesRequested -= CHUNK_SIZE;
+                                    if (numBytesRequested <= 0) {
+                                        f.addListener(QuicStreamChannel.SHUTDOWN_OUTPUT);
+                                        break;
                                     }
-                                } finally {
-                                    byteBuf.release();
+                                } while (ctx.channel().isWritable());
+                                buffer.release();
+                                ctx.flush();
+                            }
+
+                            @Override
+                            public void channelWritabilityChanged(ChannelHandlerContext ctx) {
+                                if (ctx.channel().isWritable()) {
+                                    writeData(ctx);
                                 }
                             }
                         });
@@ -112,6 +138,10 @@ public final class QuicServerExample {
             Channel channel = bs.group(group)
                     .channel(NioDatagramChannel.class)
                     .handler(codec)
+                    .option(ChannelOption.MAX_MESSAGES_PER_READ, Integer.MAX_VALUE)
+                    .option(ChannelOption.RCVBUF_ALLOCATOR, new FixedRecvByteBufAllocator(2048))
+                    .option(ChannelOption.WRITE_BUFFER_WATER_MARK,
+                            new WriteBufferWaterMark(Integer.MAX_VALUE, Integer.MAX_VALUE))
                     .bind(new InetSocketAddress(9999)).sync().channel();
             channel.closeFuture().sync();
         } finally {
